2c3f49bb4a15c76a9970f63f56317277250e9b6a
scottelsdon <46880781+scottelsdon@users.noreply.github.com>
2026-04-24 21:53:50 +1200

    Add character team photos

---
+++ download-images-v2.bat
@@ -0,0 +1,191 @@
+@echo off
+echo ================================================
+echo   Making Faces NZ - Image Downloader (Full)
+echo ================================================
+echo.
+mkdir images 2>nul
+echo Downloading images... please wait, do not close this window.
+echo.
+
+echo --- Original site images ---
+
+curl -L -A "Mozilla/5.0" -o "images\logo.jpg"           "https://makingfacesnz.art/wp-content/uploads/2018/11/cropped-making-faces-logo1.jpg"
+echo [1] logo.jpg done
+curl -L -A "Mozilla/5.0" -o "images\hero-main.jpg"      "https://makingfacesnz.art/wp-content/uploads/2026/02/462556698_1959353264567099_1419593220571123236_n.jpg"
+echo [2] hero-main.jpg done
+curl -L -A "Mozilla/5.0" -o "images\hero-float.jpg"     "https://makingfacesnz.art/wp-content/uploads/2026/02/eloise-making-faces-3.jpeg"
+echo [3] hero-float.jpg done
+curl -L -A "Mozilla/5.0" -o "images\face-painting.jpg"  "https://makingfacesnz.art/wp-content/uploads/2026/02/img_20220424_154025.jpg"
+echo [4] face-painting.jpg done
+curl -L -A "Mozilla/5.0" -o "images\balloons.jpg"       "https://makingfacesnz.art/wp-content/uploads/2023/07/bapab_l.jpg"
+echo [5] balloons.jpg done
+curl -L -A "Mozilla/5.0" -o "images\bubbles.jpg"        "https://makingfacesnz.art/wp-content/uploads/2026/02/557464020_2563637274035989_3566902146722230237_n.jpg"
+echo [6] bubbles.jpg done
+curl -L -A "Mozilla/5.0" -o "images\character-main.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/bd777d02-fd24-47d7-8877-f7fd1070d7f1-2.jpeg"
+echo [7] character-main.jpg done
+curl -L -A "Mozilla/5.0" -o "images\baby-bump.jpg"      "https://makingfacesnz.art/wp-content/uploads/2023/07/img_20230305_113155.jpg"
+echo [8] baby-bump.jpg done
+curl -L -A "Mozilla/5.0" -o "images\glitter.jpg"        "https://makingfacesnz.art/wp-content/uploads/2024/11/springintotawa24-267-x4.jpg"
+echo [9] glitter.jpg done
+curl -L -A "Mozilla/5.0" -o "images\hair-braids.jpg"    "https://makingfacesnz.art/wp-content/uploads/2024/06/img_20240317_163645.jpg"
+echo [10] hair-braids.jpg done
+curl -L -A "Mozilla/5.0" -o "images\craft.jpg"          "https://makingfacesnz.art/wp-content/uploads/2026/02/20260201_140338.jpg"
+echo [11] craft.jpg done
+curl -L -A "Mozilla/5.0" -o "images\pkg-character1.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/whatsapp-image-2025-09-25-at-11.22.54_fc672d96-2.jpg"
+echo [12] pkg-character1.jpg done
+curl -L -A "Mozilla/5.0" -o "images\pkg-character2.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/597381562_10163344581790568_6111350857729554487_n-2.jpg"
+echo [13] pkg-character2.jpg done
+curl -L -A "Mozilla/5.0" -o "images\pkg-character3.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/whatsapp-image-2025-10-31-at-19.14.11_201d79ae-2.jpg"
+echo [14] pkg-character3.jpg done
+curl -L -A "Mozilla/5.0" -o "images\char-duo1.jpg"      "https://makingfacesnz.art/wp-content/uploads/2026/02/df5655e1-ece6-464c-b3f6-a005a048d21a.jpeg"
+echo [15] char-duo1.jpg done
+curl -L -A "Mozilla/5.0" -o "images\about-main.jpg"     "https://makingfacesnz.art/wp-content/uploads/2025/04/img20240504183637.jpg"
+echo [16] about-main.jpg done
+curl -L -A "Mozilla/5.0" -o "images\about-small.jpg"    "https://makingfacesnz.art/wp-content/uploads/2025/07/20250330_111253.jpg"
+echo [17] about-small.jpg done
+curl -L -A "Mozilla/5.0" -o "images\review-claire.jpg"  "https://makingfacesnz.art/wp-content/uploads/2025/09/20250330_105516.jpg"
+echo [18] review-claire.jpg done
+curl -L -A "Mozilla/5.0" -o "images\review-amy.jpg"     "https://makingfacesnz.art/wp-content/uploads/2024/02/418500954_10161038882178828_4591131628398781293_n.jpg"
+echo [19] review-amy.jpg done
+curl -L -A "Mozilla/5.0" -o "images\review-julia.jpg"   "https://makingfacesnz.art/wp-content/uploads/2024/02/5c31c5d0-4e99-4588-b8c1-011abc3ece81.jpg"
+echo [20] review-julia.jpg done
+curl -L -A "Mozilla/5.0" -o "images\review-marnie.jpg"  "https://makingfacesnz.art/wp-content/uploads/2024/02/383301671_1705287073317698_7299561285025937920_n.jpg"
+echo [21] review-marnie.jpg done
+curl -L -A "Mozilla/5.0" -o "images\review-anca.jpg"    "https://makingfacesnz.art/wp-content/uploads/2024/02/366294035_6711187002273643_2619449405758083881_n.jpg"
+echo [22] review-anca.jpg done
+curl -L -A "Mozilla/5.0" -o "images\review-kelly.jpg"   "https://makingfacesnz.art/wp-content/uploads/2024/02/311164610_8250372175036567_3465447256870278497_n.jpg"
+echo [23] review-kelly.jpg done
+
+echo.
+echo --- Character gallery images ---
+
+curl -L -A "Mozilla/5.0" -o "images\char-g01.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/whatsapp-image-2025-09-24-at-16.10.39_585ab8ed-1.jpg"
+echo [24] char-g01.jpg done
+curl -L -A "Mozilla/5.0" -o "images\char-g02.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/whatsapp-image-2025-09-25-at-11.22.54_fc672d96-1.jpg"
+echo [25] char-g02.jpg done
+curl -L -A "Mozilla/5.0" -o "images\char-g03.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/whatsapp-image-2025-10-31-at-19.14.11_201d79ae-1.jpg"
+echo [26] char-g03.jpg done
+curl -L -A "Mozilla/5.0" -o "images\char-g04.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250928_105926.jpg"
+echo [27] char-g04.jpg done
+curl -L -A "Mozilla/5.0" -o "images\char-g05.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250330_111011.jpg"
+echo [28] char-g05.jpg done
+curl -L -A "Mozilla/5.0" -o "images\char-g06.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/img_0922.jpg"
+echo [29] char-g06.jpg done
+curl -L -A "Mozilla/5.0" -o "images\char-g07.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250330_122107.jpg"
+echo [30] char-g07.jpg done
+curl -L -A "Mozilla/5.0" -o "images\char-g08.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250409_143823.jpg"
+echo [31] char-g08.jpg done
+curl -L -A "Mozilla/5.0" -o "images\char-g09.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250330_105736.jpg"
+echo [32] char-g09.jpg done
+curl -L -A "Mozilla/5.0" -o "images\char-g10.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/7640cee9-b7b3-47d1-87f1-cc076a114e9a.jpeg"
+echo [33] char-g10.jpg done
+curl -L -A "Mozilla/5.0" -o "images\char-g11.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250727_132956.jpg"
+echo [34] char-g11.jpg done
+curl -L -A "Mozilla/5.0" -o "images\char-g12.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250330_121756.jpg"
+echo [35] char-g12.jpg done
+curl -L -A "Mozilla/5.0" -o "images\char-g13.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250727_133608.jpg"
+echo [36] char-g13.jpg done
+curl -L -A "Mozilla/5.0" -o "images\char-g14.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250409_143553.jpg"
+echo [37] char-g14.jpg done
+curl -L -A "Mozilla/5.0" -o "images\char-g15.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250727_143223.jpg"
+echo [38] char-g15.jpg done
+curl -L -A "Mozilla/5.0" -o "images\char-g16.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250727_140123.jpg"
+echo [39] char-g16.jpg done
+curl -L -A "Mozilla/5.0" -o "images\char-g17.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250727_135013.jpg"
+echo [40] char-g17.jpg done
+curl -L -A "Mozilla/5.0" -o "images\char-g18.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250411_162035.jpg"
+echo [41] char-g18.jpg done
+curl -L -A "Mozilla/5.0" -o "images\char-g19.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/whatsapp-image-2025-10-29-at-12.06.46_f05affed.jpg"
+echo [42] char-g19.jpg done
+
+echo.
+echo --- Ashlyn's character photos ---
+
+curl -L -A "Mozilla/5.0" -o "images\ashlyn-01.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250330_122111.jpg"
+echo [43] ashlyn-01.jpg done
+curl -L -A "Mozilla/5.0" -o "images\ashlyn-02.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250330_111024.jpg"
+echo [44] ashlyn-02.jpg done
+curl -L -A "Mozilla/5.0" -o "images\ashlyn-03.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250409_143639.jpg"
+echo [45] ashlyn-03.jpg done
+curl -L -A "Mozilla/5.0" -o "images\ashlyn-04.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250409_150333.jpg"
+echo [46] ashlyn-04.jpg done
+curl -L -A "Mozilla/5.0" -o "images\ashlyn-05.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250928_105926-2.jpg"
+echo [47] ashlyn-05.jpg done
+curl -L -A "Mozilla/5.0" -o "images\ashlyn-06.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250411_155415.jpg"
+echo [48] ashlyn-06.jpg done
+
+echo.
+echo --- Jess's character photos ---
+
+curl -L -A "Mozilla/5.0" -o "images\jess-01.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/whatsapp-image-2025-09-25-at-11.22.55_538a9536.jpg"
+echo [49] jess-01.jpg done
+curl -L -A "Mozilla/5.0" -o "images\jess-02.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250330_121756-1.jpg"
+echo [50] jess-02.jpg done
+
+echo.
+echo --- Caitlyn's character photos ---
+
+curl -L -A "Mozilla/5.0" -o "images\caitlyn-01.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250422_163011.jpg"
+echo [51] caitlyn-01.jpg done
+curl -L -A "Mozilla/5.0" -o "images\caitlyn-02.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250411_161000.jpg"
+echo [52] caitlyn-02.jpg done
+curl -L -A "Mozilla/5.0" -o "images\caitlyn-03.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250330_105736-1.jpg"
+echo [53] caitlyn-03.jpg done
+curl -L -A "Mozilla/5.0" -o "images\caitlyn-04.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/597381562_10163344581790568_6111350857729554487_n-1.jpg"
+echo [54] caitlyn-04.jpg done
+curl -L -A "Mozilla/5.0" -o "images\caitlyn-05.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250727_132956-1.jpg"
+echo [55] caitlyn-05.jpg done
+curl -L -A "Mozilla/5.0" -o "images\caitlyn-06.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250422_164613.jpg"
+echo [56] caitlyn-06.jpg done
+curl -L -A "Mozilla/5.0" -o "images\caitlyn-07.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250727_133608-1.jpg"
+echo [57] caitlyn-07.jpg done
+curl -L -A "Mozilla/5.0" -o "images\caitlyn-08.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250727_142326.jpg"
+echo [58] caitlyn-08.jpg done
+curl -L -A "Mozilla/5.0" -o "images\caitlyn-09.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250411_162035-1.jpg"
+echo [59] caitlyn-09.jpg done
+curl -L -A "Mozilla/5.0" -o "images\caitlyn-10.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250422_160949.jpg"
+echo [60] caitlyn-10.jpg done
+
+echo.
+echo --- Lara's character photos ---
+
+curl -L -A "Mozilla/5.0" -o "images\lara-01.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250330_110659.jpg"
+echo [61] lara-01.jpg done
+curl -L -A "Mozilla/5.0" -o "images\lara-02.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250330_121842.jpg"
+echo [62] lara-02.jpg done
+curl -L -A "Mozilla/5.0" -o "images\lara-03.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/img_8051.jpg"
+echo [63] lara-03.jpg done
+curl -L -A "Mozilla/5.0" -o "images\lara-04.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/img_8040.jpg"
+echo [64] lara-04.jpg done
+curl -L -A "Mozilla/5.0" -o "images\lara-05.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250422_160225.jpg"
+echo [65] lara-05.jpg done
+curl -L -A "Mozilla/5.0" -o "images\lara-06.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/img_0922-1.jpg"
+echo [66] lara-06.jpg done
+curl -L -A "Mozilla/5.0" -o "images\lara-07.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250422_160945.jpg"
+echo [67] lara-07.jpg done
+
+echo.
+echo --- Jaymie's character photos ---
+
+curl -L -A "Mozilla/5.0" -o "images\jaymie-01.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250727_133214.jpg"
+echo [68] jaymie-01.jpg done
+curl -L -A "Mozilla/5.0" -o "images\jaymie-02.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250409_143526.jpg"
+echo [69] jaymie-02.jpg done
+curl -L -A "Mozilla/5.0" -o "images\jaymie-03.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250727_143223-1.jpg"
+echo [70] jaymie-03.jpg done
+curl -L -A "Mozilla/5.0" -o "images\jaymie-04.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250727_135011.jpg"
+echo [71] jaymie-04.jpg done
+curl -L -A "Mozilla/5.0" -o "images\jaymie-05.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250727_141556.jpg"
+echo [72] jaymie-05.jpg done
+curl -L -A "Mozilla/5.0" -o "images\jaymie-06.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/20250727_140123-1.jpg"
+echo [73] jaymie-06.jpg done
+curl -L -A "Mozilla/5.0" -o "images\jaymie-07.jpg" "https://makingfacesnz.art/wp-content/uploads/2026/02/whatsapp-image-2025-10-29-at-12.06.46_f05affed-2.jpg"
+echo [74] jaymie-07.jpg done
+
+echo.
+echo ================================================
+echo   ALL DONE! 74 images downloaded.
+echo   Check the "images" folder next to this file.
+echo ================================================
+echo.
+pause
